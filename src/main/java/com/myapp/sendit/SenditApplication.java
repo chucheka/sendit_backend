e9f9a321c0a71ff3b0cab0e19808fac71e106dba
ucheka22 <ryanucheka@gmail.com>
2020-08-16 07:45:48 +0100

    deploy app to heroku

---
+++ src/main/java/com/myapp/sendit/SenditApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.sendit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/sendit/SenditApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myapp.sendit;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class SenditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
